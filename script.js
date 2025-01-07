034d549a799e7a032ab394bce57c8094b9de22e6
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-01-06 23:38:33 -0500

    Fix typo in subtract function and add operate function for basic math operations

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function add(a, b) {
   return a + b;
 }
 
-function subtract(a, b) {
+function substract(a, b) {
   return a - b;
 }
 
@@ -13,4 +13,27 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+//Operation variables
+
+let num1;
+let num2;
+let operator = "";
+let result;
+
+//Operate function
+
+function operate(a, b, operator) {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return substract(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else {
+    return null;
+  }
 }
