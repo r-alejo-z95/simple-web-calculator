79eb79196e7612bed23f3d2b6d129dec9739984c
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-02-17 11:59:19 -0500

    Create passNum function to pass numbers to display

--- script.js
+++ script.js
@@ -37,3 +37,25 @@ function operate(a, b, operator) {
     return null;
   }
 }
+
+//Init number buttons
+
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn6");
+const btn7 = document.querySelector("#btn7");
+const btn8 = document.querySelector("#btn8");
+const btn9 = document.querySelector("#btn9");
+
+//Init display variable
+
+const display = document.querySelector("#display");
+
+//Pass numbers to display function
+
+const passNum = (num) => {
+  display.innerText = num;
+};
